44c631abbc83725cb64c2b5fd8147c33db8616fd
min33sky <min33sky@naver.com>
2023-08-17 14:45:55 +0900

    feat: parallex effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { motion, useScroll, useSpring } from 'framer-motion';
 import './App.css';
 import Image from './components/Image';
 
 function App() {
+  const { scrollYProgress } = useScroll();
+
+  // https://www.framer.com/motion/transition/#spring
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100, // 강성
+    damping: 30, // 반동
+    restDelta: 0.001,
+  });
+
   return (
     <>
       {Array(5)
@@ -9,7 +19,12 @@ function App() {
         .map((_, i) => (
           <Image key={i} id={i + 1} />
         ))}
-      <div className="progress" />
+      <motion.div
+        className="progress"
+        style={{
+          scaleX,
+        }}
+      />
     </>
   );
 }
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,10 +1,27 @@
+import { MotionValue, motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
+
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
 export default function Image({ id }: { id: number }) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+  const y = useParallax(scrollYProgress, 300);
+
   return (
     <section>
-      <div>
-        <img src={`/${id}.jpg`} alt="image" />
+      <div ref={ref}>
+        <img src={`/${id}.webp`} alt="image" />
       </div>
-      <h2>{`#00${id}`}</h2>
+      <motion.h2
+        style={{
+          y,
+        }}
+      >{`#00${id}`}</motion.h2>
     </section>
   );
 }
