d236af2ee8c8fb27d2ec0f36d8bc180d29c150d3
min33sky <min33sky@naver.com>
2023-08-17 15:14:45 +0900

    fix

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -2,6 +2,7 @@ import { MotionValue, motion, useScroll, useTransform } from 'framer-motion';
 import { useRef } from 'react';
 
 function useParallax(value: MotionValue<number>, distance: number) {
+  //? scrollYProgress: 0 ~ 1
   return useTransform(value, [0, 1], [-distance, distance]);
 }
 
@@ -10,7 +11,7 @@ export default function Image({ id }: { id: number }) {
   const { scrollYProgress } = useScroll({
     target: ref,
   });
-  const y = useParallax(scrollYProgress, 300);
+  const y = useParallax(scrollYProgress, 1000);
 
   return (
     <section>
